3bfeb22d98108031e8d4167ce382770093f3964e
Ghost_Isaac072 <rtkjfhdc@hgtrbrfdtgfhj.ru>
2024-02-19 00:22:32 +0300

    7 commit
    Signed-off-by: Ghost_Isaac072 <rtkjfhdc@hgtrbrfdtgfhj.ru>

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('last')
+    print('as')
+    print('as')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
